b16ea9f286c773b0e7a339a791bff641c46a038a
Alley Gautam <112246176+AlleyGautam08@users.noreply.github.com>
2022-09-13 11:47:55 +0530

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>   //preprocessor directive used to include files
+// iostream= header file for taking input and printing output
+
+using namespace std;
+
+int32_t main(){
+     //main= the execution of code begins from main function (mandatory for any program)
+
+     cout <<"hello world\n";
+     // cout= used to display output in quotation marks
+}
