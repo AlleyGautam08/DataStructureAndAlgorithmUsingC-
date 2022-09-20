16213916bb485b16390064ee0ebd2642a97a1b80
Alley Gautam <112246176+AlleyGautam08@users.noreply.github.com>
2022-09-20 11:13:09 +0530

    Add files via upload

---
+++ function6.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+int hexadecimalToDecimal(string n){
+	int ans= 0;
+	int x=1;
+	int s = n.size();
+	for(int i=s-1;i>=0;i--){
+		if
+	}
+	return ans;
+}
+
+
+int main(){
+	string n;
+	cin>>n;
+	
+	cout<<hexadecimalToDecimal(n)<<endl;
+	
+	
+	
+	return 0;    
+}
