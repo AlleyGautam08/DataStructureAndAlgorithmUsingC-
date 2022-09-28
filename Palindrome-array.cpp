f8c51e967cb6a4a2d8b204ec60cc33a1ce22af7c
Alley Gautam <112246176+AlleyGautam08@users.noreply.github.com>
2022-09-28 14:54:56 +0530

    Add files via upload

---
+++ Palindrome-array.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int n;
+	cin >> n;
+	char arr[n+1];
+	cin >> arr;
+	
+	bool check = 1;
+	for(int i=0;i<n;i++){
+		if(arr[i] != arr[n-1]){
+			check = 0;
+			break;
+		}
+	}
+	if(check == true){
+		cout << "word is palindrome "<< endl;
+	}
+	else{
+		cout << "word is not palindrome "<< endl;
+	}
+	return 0;    
+} 
